570053f90627ca784c38769644d449ecea2f6835
mbc <malcolmbalch@gmail.com>
2017-12-24 21:31:32 -0500

    flask app for weight storer

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+source venv/bin/activate
+CAFFE_ROOT="/home/malcolm/Projects/caffe"
+export PYTHONPATH="$CAFFE_ROOT/distribute/python:$PYTHONPATH"
+export LD_LIBRARY_PATH="$CAFFE_ROOT/distribute/lib/"
+python app.py
