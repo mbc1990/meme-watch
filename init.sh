766f03d2a1710b5ce8f8edef83edf48a7cdfee72
mbc <malcolmbalch@gmail.com>
2017-12-31 11:07:22 -0500

    shell script for running classifier

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 CAFFE_ROOT="/home/malcolm/Projects/caffe"
 export PYTHONPATH="$CAFFE_ROOT/distribute/python:$PYTHONPATH"
 export LD_LIBRARY_PATH="$CAFFE_ROOT/distribute/lib/"
-python app.py
+python classify.py
